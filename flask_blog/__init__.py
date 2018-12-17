21913299090ef52728750525e1850cbaef63682a
Jayesh Patil <jayeshpatil@Jayesh-Patils-MacBook-Pro.local>
2018-12-17 13:36:07 -0800

    Added name to init for packaging

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 
 
+name = "flask-blog"
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
